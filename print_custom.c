5f28bf8561e8423f753a5fc7496aa338bfbc1031
khawlaAlbasheer <khawla.elbasheer@gmial.com>
2023-10-17 06:55:25 +0200

    printing a string in reverse order using recursion function

--- print_custom.c
+++ print_custom.c
@@ -47,14 +47,15 @@ int print_rev(va_list l, flags_t *f)
 	char *s = va_arg(l, char *);
 
 	(void)f;
-	/*if (!s)
-		s = "(null)";
-
-	while (s[i])
-		i++;
-
-	for (j = i - 1; j >= 0; j--)
-		_putchar(s[j]);
+	/*
+	*	if (!s)
+	*	s = "(null)";
+	*
+	*	while (s[i])
+		*	i++;
+	*
+	*	for (j = i - 1; j >= 0; j--)
+	*	_putchar(s[j]);
 	*/
 	while (s[i])
 		i++;
