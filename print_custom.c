c86747611a67328fdee70b0cf16ec536f9bfe0be
khawlaAlbasheer <khawla.elbasheer@gmial.com>
2023-10-17 06:59:01 +0200

    printing a string in reverse order

--- print_custom.c
+++ print_custom.c
@@ -47,24 +47,15 @@ int print_rev(va_list l, flags_t *f)
 	char *s = va_arg(l, char *);
 
 	(void)f;
-	/*
-	*	if (!s)
-	*	s = "(null)";
-	*
-	*	while (s[i])
-		*	i++;
-	*
-	*	for (j = i - 1; j >= 0; j--)
-	*	_putchar(s[j]);
-	*/
+	if (!s)
+		s = "(null)";
+
 	while (s[i])
 		i++;
-	if (*s == 0)
-	{
-		return (1);
-	}
-	print_rev(s + 1);
-	_putchar(*s);
+
+	for (j = i - 1; j >= 0; j--)
+		_putchar(s[j]);
+
 	return (i);
 }
 
